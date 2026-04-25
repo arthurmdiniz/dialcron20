bead47052a4fdde79f14ff34fabb0a092f6e6f9c
Arthur Marques Diniz <arthurmdiniz@outlook.com.br>
2026-04-25 16:17:28 -0300

    Fix plugin configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/dialcron20/',
+  plugins: [react()],
 })
